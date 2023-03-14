d42a479325d78cf24b59a2743b4da454680794c2
prochorov <99527782+prochorov@users.noreply.github.com>
2023-03-15 00:48:29 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,52 +1,62 @@
 #include <iostream>
-using namespace std;
-template<class T> void Bubble(T* arr, int size)
-{
-	T tmp;
-	for (int i = 0; i < size - 1; i++)
-	{
-		for (int j = 0; j < size - 1; j++)
-		{
-			if (arr[j + 1] < arr[j])
-			{
-				tmp = arr[j + 1];
-				arr[j + 1] = arr[j];
-				arr[j] = tmp;
-			}
-		}
-	}
+#include <vector>
+
+
+template <typename T>
+void merge_sort(std::vector<T>& arr) {
+    if (arr.size() <= 1) {
+        return;
+    }
+
+    std::vector<T> left, right;
+    int mid = arr.size() / 2;
+
+    for (int i = 0; i < mid; ++i) {
+        left.push_back(arr[i]);
+    }
+
+    for (int i = mid; i < arr.size(); ++i) {
+        right.push_back(arr[i]);
+    }
+
+    merge_sort(left);
+    merge_sort(right);
+
+    int i = 0, j = 0, k = 0;
+
+    while (i < left.size() && j < right.size()) {
+        if (left[i] < right[j]) {
+            arr[k] = left[i];
+            ++i;
+        } else {
+            arr[k] = right[j];
+            ++j;
+        }
+        ++k;
+    }
+
+    while (i < left.size()) {
+        arr[k] = left[i];
+        ++i;
+        ++k;
+    }
+
+    while (j < right.size()) {
+        arr[k] = right[j];
+        ++j;
+        ++k;
+    }
 }
-int main()
-{
-	setlocale(LC_ALL, "Russian");
-	int a[10] = { 3, 6, 3, 7, 9, 1, 7, 4, 5, 0 };
-	int b = 10;
-	cout << "Массив до сортировки типа int: ";
-	for (int i = 0; i < b; i++)
-	{
-		cout << a[i] << " ";
-	}
-	cout << endl;
-	Bubble(a, b);
-	cout << "Массив после сортировки типа int: ";
-	for (int i = 0; i < b; i++)
-	{
-		cout << a[i] << " ";
-	}
-	cout << endl;
-	double c[10] = { 3.2, 4.5, 0.5, 4.6, 7.6, 4.8, 2.2, 9.7, 2.4, 2.8 };
-	cout << "Массив до сортировки типа double: ";
-	for (int i = 0; i < b; i++)
-	{
-		cout << c[i] << " ";
-	}
-	cout << endl;
-	Bubble(c, b);
-	cout << "Массив после сортировки типа double: ";
-	for (int i = 0; i < b; i++)
-	{
-		cout << c[i] << " ";
-	}
-	cout << endl;
-	return 0;
+int main() {
+    std::vector<int> int_arr = {4, 2, 1, 5, 3};
+    merge_sort(int_arr);
+    for (int x : int_arr) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+
+    std::vector<double> double_arr = {3.14, 1.23, 2.71, 0.99, 2.45};
+    merge_sort(double_arr);
+    for (double x : double_arr) {
+        std::cout << x << " ";}
 }
